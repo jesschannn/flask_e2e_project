f25e68440d25f390a4472db905deb3ca46ecefa1
jesschannn <jessica.chan.3@stonybrook.edu>
2023-12-15 04:17:52 +0000

    worked on backend and frontend

--- app/app.py
+++ app/app.py
@@ -1,68 +1,77 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+import pandas as pd
+import csv
+from sqlalchemy import create_engine, inspect, text
+from pandas import read_sql
 import os
 from dotenv import load_dotenv
-from pandas import read_sql
-from sqlalchemy import create_engine, text
+from flask_sqlalchemy import SQLAlchemy
+from authlib.integrations.flask_client import OAuth
+from authlib.common.security import generate_token
+from flask_session import Session
+import sentry_sdk
 
-load_dotenv()
+app = Flask(__name__)
 
-# Database connection settings from environment variables
 DB_HOST = os.getenv("DB_HOST")
 DB_DATABASE = os.getenv("DB_DATABASE")
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
+DB_NAME = os.getenv("DB_NAME")
+DB_PORT = int(os.getenv("DB_PORT", "3306"))
 DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
 
-# Connection string
-connect_args={'ssl':{'fake_flag_to_enable_tls': True}}
-
-connection_string = (f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}'
-                    f"?charset={DB_CHARSET}")
-
-engine = create_engine(
-        connection_string,
-        connect_args=connect_args,
-
-)
-
-app = Flask(__name__, template_folder='templates')
+load_dotenv()
 
 @app.route('/')
 def index():
     return render_template('base.html')
 
-@app.route('/patients')
-def patients():
-    # Establish a database connection
-    with engine.connect() as connection:
-        # Execute an SQL query to fetch data (replace this with your query)
-        query1 = text('SELECT * FROM patients')
-
-        result1 = connection.execute(query1)
-
-        # Fetch all rows of data
-        db_data1 = result1.fetchall()
-
-    return render_template('patients.html', data1=db_data1)
-
-@app.route('/behavioralhealthservices')
-def behavioral_health_services():
-    # Establish a database connection
-    with engine.connect() as connection:
-        # Execute an SQL query to fetch data (replace this with your query)
-        query2 = text('SELECT * FROM behavioralhealthservices')
-
-        result2 = connection.execute(query2)
-
-        # Fetch all rows of data
-        db_data2 = result2.fetchall()
-
-    return render_template('behavioral_health_services.html', data2=db_data2)
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/importance')
-def importance():
+def mental():
     return render_template('importance.html')
 
+# Load data from CSV file
+df = pd.read_csv('/home/jessica_chan_3/flask_e2e_project/data/clean_medicalmalpractice.csv')
+
+@app.route('/data')
+def display_csv():
+    data = []
+    with open('/home/jessica_chan_3/flask_e2e_project/data/clean_medicalmalpractice.csv', newline='') as csvfile:
+        csvreader = csv.reader(csvfile)
+        for row in csvreader:
+            data.append(row)
+
+    # Retrieve a sample of 50 rows from the data
+    sample_data = data[:50]
+
+    return render_template('data.html', data=sample_data)
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/submit_contact', methods=['POST'])
+def submit_contact():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    phone_number = request.form.get('phone_number')
+    message = request.form.get('message')
+
+    return redirect(url_for('thank_you'))
+
+@app.route('/thank_you')
+def thank_you():
+    return render_template('thank_you.html')
+
+@app.route('/error')
+def error():
+    raise Exception('Error!')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
