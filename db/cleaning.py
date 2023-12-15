777e8af4ee408b32929e54bfc0e66772eaeee560
jesschannn <jessica.chan.3@stonybrook.edu>
2023-12-15 04:18:50 +0000

    worked on backend and frontend

--- db/cleaning.py
+++ db/cleaning.py
@@ -2,22 +2,16 @@ import pandas as pd
 import re
 
 # Loading in Dataset
-df = pd.read_csv('data/behavior_health.csv')
+df = pd.read_csv('data/medicalmalpractice.csv')
 
 # Making Column Names Lowercase
 df.columns = map(str.lower, df.columns)
 
-# Dropping a Column
-column_to_remove = 'dataquality'
-df = df.drop(column_to_remove, axis=1)
-
-# Dropping Unknown Values
-servicecount_value_counts = df['servicecount'].value_counts()
-rate_value_counts = df['rateper1000beneficiaries'].value_counts()
-
-df = df[df['servicecount'] != 'DQ']
-df = df[df['servicecount'] != 'DS']
-df = df[df['rateper1000beneficiaries'] != 'DQ']
+# Removing Space in Column Name
+df.columns = df.columns.map(lambda x: x.strip().replace(' ', '_'))
 
 # Dropping Missing Values
 df = df.dropna
+
+# Save Cleaned CSV File
+df.to_csv('clean_medicalmalpractice.csv', index=False)
