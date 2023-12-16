9ff812eded66e293f0c8259c46475b6951ef870f
jesschannn <jessica.chan.3@stonybrook.edu>
2023-12-16 23:14:12 +0000

    added css and worked on sql + oauth

--- app/app.py
+++ app/app.py
@@ -10,18 +10,33 @@ from authlib.integrations.flask_client import OAuth
 from authlib.common.security import generate_token
 from flask_session import Session
 import sentry_sdk
+from oauth.db_functions import update_or_create_user
+
+GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
 
 app = Flask(__name__)
+app.secret_key = os.urandom(12)
+oauth = OAuth(app)
 
-DB_HOST = os.getenv("DB_HOST")
+load_dotenv()  # Load environment variables from .env file
+
+# Database connection settings from environment variables
+DB_HOSTNAME = os.getenv("DB_HOSTNAME")
 DB_DATABASE = os.getenv("DB_DATABASE")
-DB_USERNAME = os.getenv("DB_USERNAME")
+DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
 DB_PORT = int(os.getenv("DB_PORT", "3306"))
 DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
 
-load_dotenv()
+# Connection string
+conn_string = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOSTNAME}:{DB_PORT}/{DB_DATABASE}"
+    "?charset=utf8mb4"
+)
+
+# Database connection settings
+db_engine = create_engine(conn_string, pool_pre_ping=True)
 
 @app.route('/')
 def index():
@@ -51,6 +66,14 @@ def display_csv():
 
     return render_template('data.html', data=sample_data)
 
+@app.route('/sql')
+def sql():
+    query_medical_malpractice = "SELECT * FROM medical_malpractice"
+    df_medical_malpractice = read_sql(query_medical_malpractice, db_engine)
+    data_medical_malpractice = df_medical_malpractice.to_dict(orient='records')
+    
+    return render_template('sql.html', data_medical_malpractice=data_medical_malpractice)
+
 @app.route('/api', methods=['GET'])
 def api_get():
     name = request.args.get('name', 'nothing')
@@ -65,6 +88,52 @@ def api_post():
     
     name = data.get('name', 'nothing')
     return jsonify({'message': f'Hello {name}! Welcome to my Medical Malpractice Web App!'})
+
+@app.route('/google/')
+def google():
+    CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+    oauth.register(
+        name='google',
+        client_id=GOOGLE_CLIENT_ID,
+        client_secret=GOOGLE_CLIENT_SECRET,
+        server_metadata_url=CONF_URL,
+        client_kwargs={
+            'scope': 'openid email profile'
+        }
+    )
+
+    # Redirect to google_auth function
+    ###note, if running locally on a non-google shell, do not need to override redirect_uri
+    ### and can just use url_for as below
+    redirect_uri = url_for('google_auth', _external=True)
+    print('REDIRECT URL: ', redirect_uri)
+    session['nonce'] = generate_token()
+    ##, note: if running in google shell, need to override redirect_uri 
+    ## to the external web address of the shell, e.g.,
+    redirect_uri = 'https://5000-cs-749231008733-default.cs-us-east1-vpcf.cloudshell.dev/google/auth/'
+    return oauth.google.authorize_redirect(redirect_uri, nonce=session['nonce'])
+
+@app.route('/google/auth/')
+def google_auth():
+    token = oauth.google.authorize_access_token()
+    user = oauth.google.parse_id_token(token, nonce=session['nonce'])
+    session['user'] = user
+    update_or_create_user(user)
+    print(" Google User ", user)
+    return redirect('/dashboard')
+
+@app.route('/dashboard/')
+def dashboard():
+    user = session.get('user')
+    if user:
+        return render_template('dashboard.html', user=user)
+    else:
+        return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
